6e25b6633b55f082786c85e4c1bccefe1f0ab94d
CelsoDeCarvalho <celsomomade361@gmail.com>
2020-12-01 00:51:22 +0200

    actualizado v1

--- app/src/main/java/com/sidratech/gochat/TelaNovoUsuario.java
+++ app/src/main/java/com/sidratech/gochat/TelaNovoUsuario.java
@@ -1,20 +1,31 @@
 package com.sidratech.gochat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+
 public class TelaNovoUsuario extends AppCompatActivity {
 
     private EditText name,email,password;
-    private Button reg_button;
+    private Button reg_button,photo_select_btn;
+    private Uri foto_uri;
+    private ImageView foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,8 @@ public class TelaNovoUsuario extends AppCompatActivity {
         email=findViewById(R.id.edi_email);
         password=findViewById(R.id.edit_password);
         reg_button=findViewById(R.id.btnReg);
+        photo_select_btn=findViewById(R.id.btn_foto);
+        foto=findViewById(R.id.photo);
     }
 
     private void action(){
@@ -36,6 +49,13 @@ public class TelaNovoUsuario extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 newUser();
+            }
+        });
+
+        photo_select_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectPhoto();
             }
         });
     }
@@ -50,4 +70,26 @@ public class TelaNovoUsuario extends AppCompatActivity {
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(e_mail,pass);
     }
 
+    private void selectPhoto(){
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==0){
+            foto_uri=data.getData();
+            Bitmap bitmap=null;
+            try {
+                bitmap=MediaStore.Images.Media.getBitmap(getContentResolver(),foto_uri);
+                foto.setImageDrawable(new BitmapDrawable(bitmap));
+                photo_select_btn.setAlpha(0);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
